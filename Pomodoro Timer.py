bce4fee88177d532c198b88fb67140332a493d22
msicrow <michaelscrow@gmail.com>
2020-04-29 20:09:59 +0100

    Updated comments.

--- Pomodoro Timer.py
+++ Pomodoro Timer.py
@@ -5,13 +5,13 @@ import time
 
 class Timer:
     def __init__(self, parent):
-        self.time_start = datetime.now()
-        self.time_end = self.time_start + timedelta(seconds=0) # Pomodoro technique typically 25 minute bursts
+        self.time_start = datetime.now()  # Uses system time.
+        self.time_end = self.time_start + timedelta(seconds=1500)  # Pomodoro technique typically 25 minute bursts.
         self.time_delta = timedelta()
 
-        self.active = False
+        self.active = False  # Tracks whether timer is counting down.
 
-        self.run_count = 0
+        self.run_count = 0  # Differentiates break and work cycles.
 
         self.container = Frame(parent)
         self.container.pack()
@@ -19,7 +19,7 @@ class Timer:
         self.time_display = Label(parent, text="25:00")
         self.time_display.pack()
 
-        self.time_state = Label(parent, text="Work")
+        self.time_state = Label(parent, text="Work") 
         self.time_state.pack()
 
         self.start_button = Button(self.container)
@@ -42,20 +42,20 @@ class Timer:
                 self.time_display.after(500, self.countdown)
             else:
                 time_countdown = self.time_end - datetime.now()
-                time_countdown = time_countdown.seconds
-                minutes, seconds = divmod(time_countdown, 60)
-                self.time_display.configure(text=("%02d:%02d" % (int(minutes), int(seconds))))
+                time_countdown = time_countdown.seconds  # Converts to seconds.
+                minutes, seconds = divmod(time_countdown, 60)  # Seconds converted to minutes & seconds.
+                self.time_display.configure(text=("%02d:%02d" % (int(minutes), int(seconds))))  # Replaces time text.
                 self.time_display.after(500, self.countdown)
 
     def break_time(self):
         if self.run_count == 1:
             self.time_state["text"] = "Work"
-            self.time_end = datetime.now() + timedelta(seconds=1500)
+            self.time_end = datetime.now() + timedelta(seconds=1500)  # Restarts "Work" cycle.
             self.time_display.after(500, self.countdown)
             self.run_count = 0
         else:
             self.time_state["text"] = "Break"
-            self.time_end = datetime.now() + timedelta(seconds=300)
+            self.time_end = datetime.now() + timedelta(seconds=300)  # 5 minute break.
             self.run_count += 1
             self.time_display.after(500, self.countdown)
 
@@ -64,22 +64,22 @@ class Timer:
         self.time_end = datetime.now() + timedelta(seconds=1500)
         self.countdown()
         self.start_button["text"] = "Pause"
-        self.start_button.bind("<Button-1>", self.pause_click)
+        self.start_button.bind("<Button-1>", self.pause_click)  # LMB binds to different function each click.
 
     def pause_click(self, event):
         self.start_button["text"] = "Resume"
         self.start_button.bind("<Button-1>", self.resume_click)
-        self.time_delta = self.time_end - datetime.now()   # difference between time now and start time
+        self.time_delta = self.time_end - datetime.now()   # Time delta at point of pause_click.
 
     def resume_click(self, event):
         self.active = True
         self.start_button["text"] = "Pause"
         self.start_button.bind("<Button-1>", self.pause_click)
-        self.time_end = datetime.now() + self.time_delta   # adding difference to end time
+        self.time_end = datetime.now() + self.time_delta   # Adding time delta to end time upon resume_click.
 
     def reset_click(self, event):
         self.active = False
-        self.time_display.configure(text="25:00")
+        self.time_display.configure(text="25:00")  # Resets display text.
         self.time_end = datetime.now() + timedelta(seconds=1500)
         self.start_button["text"] = "Start"
         self.start_button.bind("<Button-1>", self.start_click)
@@ -87,6 +87,6 @@ class Timer:
 
 if __name__ == "__main__":
     root = Tk()
-    root.attributes("-topmost", True)
+    root.attributes("-topmost", True)  # Brings window to front.
     timer = Timer(root)
     root.mainloop()
